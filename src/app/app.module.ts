62a6743bb4ed8ac4b851682962d6903f1eee667d
Muni <muni2intell@gmail.com>
2021-05-12 09:36:51 +0530

    otp package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 
+// Npm Packages
+import { NgOtpInputModule } from  'ng-otp-input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgOtpInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
